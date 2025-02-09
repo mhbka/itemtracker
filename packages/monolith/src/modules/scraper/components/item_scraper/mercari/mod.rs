7138994d5b018f353384240a5587f980a83bcd9c
M Hisham <mhishambka@gmail.com>
2025-02-10 03:20:07 +0800

    progress on mercari scraper porting

--- packages/monolith/src/modules/scraper/components/item_scraper/mercari/mod.rs
+++ packages/monolith/src/modules/scraper/components/item_scraper/mercari/mod.rs
@@ -1,4 +1,8 @@
+use futures::future::join_all;
 use reqwest::Client;
+use crate::{galleries::{domain_types::ItemId, items::item_data::MarketplaceItemData}, modules::scraper::components::utils::generate_dpop::generate_dpop};
+
+const REQ_URL: &str = "https://api.mercari.jp/items/get"; // TODO: move to config
 
 /// This struct is in charge of scraping items from Mercari.
 pub(super) struct MercariItemScraper {
@@ -12,7 +16,46 @@ impl MercariItemScraper {
         }
     }
 
-    pub fn request(&self, item_ids: Vec<ItemId>) -> Result<String, String> {
-        todo!()
+    /// Attempt to scrape a list of item IDs,
+    /// returning a list with (in order) the item's data, or an `Err` if the scrape wasn't successful.
+    /// 
+    /// Returns the `Vec` with a single `Err` if the *dpop* key generation was unsuccessful (should never happen).
+    pub async fn request(&self, item_ids: Vec<ItemId>) -> Vec<Result<MarketplaceItemData, String>> {
+        let dpop_key = match generate_dpop(&REQ_URL, "get") {
+            Ok(key) => key,
+            Err(err) => return vec![Err(err)]
+        };
+        let request_futures = item_ids
+            .into_iter()
+            .map(|id| {
+            let dpop_key = dpop_key.clone();
+            async move {
+                self.client
+                    .get(format!("{REQ_URL}?id={id}"))
+                    .header("dpop", dpop_key)
+                    .header("x-platform", "web") // TODO: see if these 2 are actually necessary
+                    .header("content-type", "application/json")
+                    .send()
+                    .await
+            }
+            });
+        let responses = join_all(request_futures).await;
+        self.handle_responses(responses)
     }    
+
+    /// Parses the raw responses from the item scrape. 
+    fn handle_responses(&self, responses: Vec<Result<reqwest::Response, reqwest::Error>>) -> Vec<Result<MarketplaceItemData, String>> {
+        responses.into_iter()
+            .map(|response| {
+                match response {
+                    Ok(res) => {
+
+                    },
+                    Err(err) =>  {
+                        
+                    }
+                }   
+            })
+            .collect()
+    }
 }
