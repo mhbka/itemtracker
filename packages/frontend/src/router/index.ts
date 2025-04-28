6262f1e892fe1a7f946d990c9f5f0a67d8ada3c8
M Hisham <jamneptune@gmail.com>
2025-04-29 00:51:49 +0800

    fuck it change to hash mode

--- packages/frontend/src/router/index.ts
+++ packages/frontend/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DashboardView from '../views/DashboardView.vue'
 import GalleryView from '../views/GalleryView.vue'
@@ -8,7 +8,7 @@ import { supabase } from '../main'
 import NewGalleryView from '@/views/NewGalleryView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
